23539feb72a91cb9e3f8c03fa3a1df9368d98d9a
Pierce Dreiling <piercedreiling@gmail.com>
2024-09-24 15:03:50 -0500

    added foreign key constraints

--- app/src/main/java/com/example/inventory/data/ListMovies.kt
+++ app/src/main/java/com/example/inventory/data/ListMovies.kt
@@ -1,9 +1,28 @@
 package com.example.inventory.data
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
 
 /* associative entity between List and Movie ; many-to-many relationship*/
-@Entity(tableName = "list_movies", primaryKeys = ["listName", "movieID"])
+@Entity(tableName = "list_movies",
+    primaryKeys = ["listName", "movieID"],
+    foreignKeys = [
+        ForeignKey(
+            entity = UserList::class, // reference to UserList entity
+            parentColumns = ["listName"], // PK in UserList
+            childColumns = ["listName"], // FK in here, ListMovies
+            onDelete = ForeignKey.CASCADE, // cascade deletes and updates from UserList to here
+            onUpdate = ForeignKey.CASCADE
+        ),
+        ForeignKey(
+            entity = Movie::class, // reference to Movie entity
+            parentColumns = ["movieID"], // PK in Movie
+            childColumns = ["movieID"], // FK in here, ListMovies
+            onDelete = ForeignKey.CASCADE, // cascade deletes and updates from Movie to here
+            onUpdate = ForeignKey.CASCADE
+        )
+    ]
+)
 data class ListMovies(
     val listName: String,
     val movieID: Int // TMDB id of a movie
--- app/src/main/java/com/example/inventory/data/ListShows.kt
+++ app/src/main/java/com/example/inventory/data/ListShows.kt
@@ -1,9 +1,28 @@
 package com.example.inventory.data
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
 
 /* associative entity between Lists and Shows ; many-to-many relationship*/
-@Entity(tableName = "list_shows", primaryKeys = ["listName", "showID"])
+@Entity(tableName = "list_shows",
+    primaryKeys = ["listName", "showID"],
+    foreignKeys = [
+        ForeignKey(
+            entity = UserList::class, // reference to UserList entity
+            parentColumns = ["listName"], // PK in UserList
+            childColumns = ["listName"], // FK in here, ListMovies
+            onDelete = ForeignKey.CASCADE, // cascade deletes and updates from UserList to here
+            onUpdate = ForeignKey.CASCADE
+        ),
+        ForeignKey(
+            entity = Show::class, // reference to Show entity
+            parentColumns = ["showID"], // PK in Show
+            childColumns = ["showID"], // FK in here, ListShows
+            onDelete = ForeignKey.CASCADE, // cascade deletes and updates from Show to here
+            onUpdate = ForeignKey.CASCADE
+        )
+    ]
+)
 data class ListShows(
     val listName: String,
     val showID: Int // TMDB id of a show
