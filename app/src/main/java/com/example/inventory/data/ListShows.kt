0bec3aa759bb0ff7514bd719b1640eb80520f96d
Pierce Dreiling <piercedreiling@gmail.com>
2024-09-24 13:10:27 -0500

    fixed bug with primary key

--- app/src/main/java/com/example/inventory/data/ListShows.kt
+++ app/src/main/java/com/example/inventory/data/ListShows.kt
@@ -3,7 +3,7 @@ package com.example.inventory.data
 import androidx.room.Entity
 
 /* associative entity between Lists and Shows ; many-to-many relationship*/
-@Entity(tableName = "list_shows", primaryKeys = ["listName, showID"])
+@Entity(tableName = "list_shows", primaryKeys = ["listName", "showID"])
 data class ListShows(
     val listName: String,
     val showID: Int // TMDB id of a show
