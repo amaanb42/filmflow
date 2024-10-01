eff07a735934611b1eeca509d7cbbd65130880d5
Amaan Bari <abaricomp@gmail.com>
2024-10-01 13:50:36 -0500

    more changes to movie api classes

--- app/src/main/java/com/example/inventory/data/api/MovieDetails.kt
+++ app/src/main/java/com/example/inventory/data/api/MovieDetails.kt
@@ -3,9 +3,9 @@ package com.example.inventory.data.api
 data class MovieDetails (
     val id: Int,
     val title: String,
-    val overview: String,
-    val posterPath: String,
+    val overview: String, //synopsis
+    val posterPath: String, //movie poster
     val releaseDate: String,
-    val rating: Double,
-    val popularity: Double
+    val runtime: Int,
+    val rating: Double //vote_average
 )
