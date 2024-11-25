c68fc3900602f78146e09288ef7766fcd7e120c9
amaanb <abaricomp@gmail.com>
2024-11-25 14:08:13 -0600

    figure out how to properly clip rating circle

--- app/src/main/java/com/example/inventory/ui/home/LocalDetailScreen.kt
+++ app/src/main/java/com/example/inventory/ui/home/LocalDetailScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
@@ -44,6 +45,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
@@ -217,32 +219,14 @@ fun LocalMovieDetailsScreen(navController: NavHostController, movieId: Int) {
                         Spacer(modifier = Modifier.height(8.dp)) // Increased spacing
                         Row( // TODO: make prettier
                             modifier = Modifier
-                                .clickable { showChangeRatingDialog = true } // display the dialog
                                 .fillMaxWidth()
                         ) {
-                            Spacer(modifier = Modifier.width(10.dp))
-//                            Icon(
-//                                imageVector = Icons.Filled.Star,
-//                                contentDescription = "Star Icon",
-//                                modifier = Modifier.size(40.dp),
-//                                tint = light_gold
-//                            )
-//                            Spacer(modifier = Modifier.width(10.dp))
-//                            Text(
-//                                text = String.format("%.1f", movie?.userRating ?: 0.0), // Format with one decimal place
-//                                style = MaterialTheme.typography.bodyMedium,
-//                                fontSize = 20.sp,
-//                                modifier = Modifier.align(Alignment.CenterVertically)
-//                            )
-//                            Text(
-//                                text = " / 10",
-//                                style = MaterialTheme.typography.bodyMedium,
-//                                fontSize = 16.sp,
-//                                modifier = Modifier.align(Alignment.CenterVertically).padding(top = 2.dp),
-//                                color = Color.Gray
-//                            )
+                            //Spacer(modifier = Modifier.width(10.dp))
                             Box(
-                                Modifier.align(Alignment.CenterVertically).padding(top = 24.dp, start = 12.dp)
+                                Modifier.align(Alignment.CenterVertically).padding(top = 36.dp, start = 24.dp)
+                                    //.clip(CircleShape)
+                                    //.size(100.dp)
+                                    .clickable { showChangeRatingDialog = true } // display the dialog
                             ) {
                                 CircularProgressBar(userRating = movie?.userRating ?: 0.0f)
                             }
@@ -414,7 +398,10 @@ fun CircularProgressBar(
         contentAlignment = Alignment.Center,
         modifier = Modifier.size(radius * 2f)
     ) {
-        Canvas(modifier = Modifier.size(radius * 2f)) {
+        Canvas(
+            modifier = Modifier.size(radius * 2f)
+                //.clip(CircleShape)
+        ) {
             // Draw the white circle first
             drawCircle(
                 color = Color.White,
