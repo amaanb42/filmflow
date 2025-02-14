8e20ec19faa56c04450b2064f893ed034c03166d
amaanb <abaricomp@gmail.com>
2025-02-14 14:53:10 -0600

    database version increment

--- app/src/main/java/com/example/inventory/data/AppDatabase.kt
+++ app/src/main/java/com/example/inventory/data/AppDatabase.kt
@@ -22,7 +22,7 @@ import kotlinx.coroutines.launch
 
 @Database(
     entities = [UserList::class, Movie::class, Show::class, ListMovies::class, ListShows::class],
-    version = 1,
+    version = 3,
     exportSchema = false
 )
 @TypeConverters(Converters::class)
