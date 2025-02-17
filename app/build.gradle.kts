f157d8156fb14200afee5d84ff0e0a14692a9ccc
amaanb <abaricomp@gmail.com>
2025-02-17 15:41:22 -0600

    release build no longer debuggable

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = true
-            isDebuggable = true
+            isDebuggable = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
--- app/src/main/java/com/example/inventory/InventoryApplication.kt
+++ app/src/main/java/com/example/inventory/InventoryApplication.kt
@@ -3,7 +3,6 @@ package com.example.inventory
 import android.app.Application
 import android.content.Context
 import com.example.inventory.data.AppDataContainer
-import com.example.inventory.data.AppDatabase
 
 class InventoryApplication : Application() {
 
@@ -13,21 +12,10 @@ class InventoryApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         container = AppDataContainer(this)
-        // Consider moving database initialization to a background thread
-        // (though, as mentioned, Room often handles this well enough).
-        //  initializeDatabase()
         instance = this //set up the companion object
     }
     companion object{
         private lateinit var instance: InventoryApplication
         fun getContext(): Context = instance.applicationContext
     }
-    /*
-    // OPTIONAL: Example of background database initialization (using a coroutine):
-    private fun initializeDatabase() {
-        CoroutineScope(Dispatchers.IO).launch {
-            AppDatabase.getDatabase(this@InventoryApplication).openHelper.writableDatabase
-        }
-    }
-    */
 }
