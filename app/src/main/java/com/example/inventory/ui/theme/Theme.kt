536238bd0bef296f43b88ebd41f8b9da118ceb30
amaanb <abaricomp@gmail.com>
2025-02-22 15:57:52 -0600

    theme update

--- app/src/main/java/com/example/inventory/ui/theme/Theme.kt
+++ app/src/main/java/com/example/inventory/ui/theme/Theme.kt
@@ -1,7 +1,5 @@
 package com.example.inventory.ui.theme
 
-import android.app.Activity
-import android.graphics.Color
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
@@ -9,10 +7,7 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = dark_pine,
@@ -80,23 +75,6 @@ fun FilmFlowTheme(
         }
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
-    }
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        SideEffect {
-            val window = (view.context as Activity).window
-            // Enable edge-to-edge
-            WindowCompat.setDecorFitsSystemWindows(window, false)
-
-            // Set transparent system bars
-            window.statusBarColor = Color.TRANSPARENT
-            window.navigationBarColor = Color.TRANSPARENT
-
-            // Adjust system bar icons for light/dark theme
-            val insetsController = WindowCompat.getInsetsController(window, view)
-            insetsController.isAppearanceLightStatusBars = !darkTheme
-            insetsController.isAppearanceLightNavigationBars = !darkTheme
-        }
     }
 
     MaterialTheme(
