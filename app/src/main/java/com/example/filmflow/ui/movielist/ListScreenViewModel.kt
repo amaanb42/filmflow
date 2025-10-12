8916d3926ea5003bad8353677f0638adbbfa15b8
amaanb <abaricomp@gmail.com>
2025-10-11 22:12:21 -0400

    removed unnecessary functions

--- app/src/main/java/com/example/filmflow/ui/movielist/ListScreenViewModel.kt
+++ app/src/main/java/com/example/filmflow/ui/movielist/ListScreenViewModel.kt
@@ -12,10 +12,9 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.stateIn
-import kotlinx.coroutines.launch
 
 class ListScreenViewModel(
-    private val userListRepository: UserListRepository,
+    userListRepository: UserListRepository,
     private val listMoviesRepository: ListMoviesRepository,
     private val movieRepository: MovieRepository
 ) : ViewModel() {
@@ -28,13 +27,6 @@ class ListScreenViewModel(
 
     // used for displaying in modal bottom sheet, no need to pull from db
     val defaultLists: List<UserList> = listOf(UserList("Planning"), UserList("Watching"), UserList("Completed"))
-
-    // StateFlow for displaying all lists in the bottom screen sheet
-    val allLists: StateFlow<List<UserList>> = userListRepository.getAllListsStream().stateIn(
-        scope = viewModelScope,
-        started = SharingStarted.WhileSubscribed(5000),
-        initialValue = emptyList()
-    )
 
     // gets movie counts for default lists, only way i could think of doing this.
     val completedCount: StateFlow<Int> = userListRepository.getMovieCountStream("Completed").stateIn(
@@ -86,36 +78,6 @@ class ListScreenViewModel(
         }
     }
 
-    // function to add new list to db
-    fun addNewList(listName: String) {
-        viewModelScope.launch {
-            userListRepository.insertList(UserList(listName=listName))
-        }
-    }
-
-    // function to delete list from db
-    fun deleteList(listName: String) {
-        viewModelScope.launch {
-            userListRepository.deleteListByName(listName=listName)
-        }
-    }
-
-    // function to edit list name in db
-    fun renameList(oldName: String, newName: String) {
-        viewModelScope.launch {
-            userListRepository.updateListByName(oldName=oldName, newName=newName)
-        }
-    }
-
-    var isInList = false
-    // function to check if new list name exists
-    fun newListNameExists(oldName: String?, newName: String) {
-        viewModelScope.launch {
-            val updatedList = allLists.value.filterNot {it.listName == oldName}
-            isInList = updatedList.any {it.listName == newName}
-        }
-    }
-
 }
 // pass the repository to ListScreenViewModel
 class ListScreenViewModelFactory(
@@ -123,6 +85,7 @@ class ListScreenViewModelFactory(
     private val listMoviesRepository: ListMoviesRepository,
     private val movieRepository: MovieRepository
 ) : ViewModelProvider.Factory {
+    @Suppress("UNCHECKED_CAST")
     override fun <T: ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(ListScreenViewModel::class.java)) {
             return ListScreenViewModel(userListRepository, listMoviesRepository, movieRepository) as T
